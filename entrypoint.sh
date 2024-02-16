0d2c4cd668ef06235a04616f1d4a762c891bedf5
Anthony Grasso <anthony.grasso@gmail.com>
2024-02-17 10:41:18 +1100

    Allow CDM property values to have spaces
    * Fixed bug where the entrypoint would exit with a generic message when grep failed to find the property in the CDM properties file, instread of reporting it was unable to find the property specified
    * Fixed bug where property values with spaces would cause the entrypoint to fail
    * Added the ability to pass an argument so that the entrypoint will exit after it has successfully completed its operations

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,13 @@
 
 set -euo pipefail
 
+# Sets what to do after entrypoint has completed all its operations. By default we will wait indefinitely after we have
+# completed all the operations in the entrypoint.
+COMPLETION_ACTION="wait"
+IFS=$'\n'
 ERR_MSG=""
 
-# Catch when we are about to exit and display the error
+# Catch when we are about to exit and display the error.
 trap "trap_exit" EXIT
 
 
@@ -35,7 +39,7 @@ trap_exit() {
 
   if [ -n "$err_msg" ]
   then
-    err_msg_out="$err_msg_out $err_msg. "
+    err_msg_out="$err_msg. "
   fi
 
   if [ "$(cut -d' ' -f1 <<<"$BASH_COMMAND")" = "exit" ]
@@ -53,7 +57,8 @@ set_operating_file_values() {
   local file_path="$1"
   local env_var_prefix="$2"
   local delimiter="$3"
-  local delimiter_regex="[\ ]+"
+  local delimiter_regex_match="[\ ]+"
+  local delimiter_regex_replace="\2"
 
   env_config_values=("$(env | grep "$env_var_prefix" || echo '')")
 
@@ -65,7 +70,9 @@ set_operating_file_values() {
 
   if [ -n "$delimiter" ]
   then
-    delimiter_regex="[\ ]*${delimiter}[\ ]*"
+    delimiter_regex_match="[\ ]*${delimiter}[\ ]*"
+  else
+    delimiter=" "
   fi
 
   info "Updating settings in $file_path"
@@ -81,7 +88,9 @@ set_operating_file_values() {
         new_conf_val="$temp_conf_val"
     fi
 
-    conf_line=$(grep -i -e "^[#]*${conf_key}" "$file_path")
+    # Use true to catch the case where the conf_key is not found in the file, otherwise grep will return a non-zero
+    # exit code and cause the container to exit without a useful error message.
+    conf_line=$(grep -i -e "^[#]*${conf_key}" "$file_path" || true)
     if [ -z "$conf_line" ]
     then
       error_exit "Unable to find property $conf_key in $file_path"
@@ -94,16 +103,28 @@ set_operating_file_values() {
       info_msg=" - Updating"
     fi
 
+    # Check if the line contains spaces and a value after the property key. If there is no space and value, then remove
+    # the delimiter matching and set the delimiter replacement to eight spaces.
+    if [ "$(tr -s ' ' <<<"$conf_line" | cut -d"$delimiter" -f2 | tr -d ' ')" = "$conf_line" ]
+    then
+      delimiter_regex_match=""
+      delimiter_regex_replace="        "
+    fi
+
     info_msg="$info_msg property $conf_key"
 
     # perform a case insensitive search and replace as the conf_key is all lower case and the CDM Properties file
     # contains camel case properties.
-    sed -i -E "s;^[#]?($conf_key)($delimiter_regex).*;\1\2${new_conf_val};i" "$file_path"
+    sed -i -E "s;^[#]?($conf_key)($delimiter_regex_match).*;\1${delimiter_regex_replace}${new_conf_val};i" "$file_path"
 
     info "${info_msg}"
 
     done
 }
+
+# Have the option to override the default completion behaviour incase we want to run the entrypoint manually from
+# within the container.
+[ "$#" -gt 0 ] && COMPLETION_ACTION="$1"
 
 info "Setting up Cassandra Data Migrator $CDM_VERSION"
 info "Using Java $JAVA_VERSION"
@@ -114,4 +135,10 @@ set_operating_file_values "$CDM_PROPERTIES_FILE" "CDM_PROPERTY_" ""
 set_operating_file_values "$CDM_LOG4J_CONFIGURATION" "CDM_LOGGING_" "="
 
 info "Ready to run Cassandra Data Migrator. Run spark-submit-cdm to start the migration."
+
+if [ "$COMPLETION_ACTION" = "exit" ]
+then
+  exit 0
+fi
+
 tail -f /dev/null
